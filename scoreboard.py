33cee1ebd402a86c9a84b73e74acd4b6e70256a4
SbongiseniMbaraga <sbongiseni2@hotmail.com>
2021-02-15 15:41:32 +0200

    final update

--- scoreboard.py
+++ scoreboard.py
@@ -17,14 +17,22 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.get_num()}", align=ALIGNMENT, font=FONT)
 
     def reset(self):
-        if self.score > self.high_score:
+        if self.score > self.get_num():
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(str(self.high_score))
         self.score = 0
         self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
         self.update_scoreboard()
+
+    @staticmethod
+    def get_num():
+        with open("data.txt") as data:
+            number = data.read()
+        return int(number)
